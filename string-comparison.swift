03f33180e67c8d7eb2613809be18163add55e43c
Panayiotis Georgiou <georgioupanayiotis@users.noreply.github.com>
2018-03-27 15:21:01 +0100

    Create string-comparison.swift

---
+++ string-comparison.swift
@@ -0,0 +1,9 @@
+let password = "HelloWorld"
+let repeatPassword = "HelloWorld"
+
+if ((password.elementsEqual(repeatPassword)) == true)
+{
+   print("Passwords are equal")
+} else {
+   print("Passwords are not equal")
+}
